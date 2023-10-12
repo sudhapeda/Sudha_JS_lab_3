6862a74a11f1b0664e788b017548bcbf27405d01
sudha kumari <96000707+sudhapeda@users.noreply.github.com>
2023-10-12 14:54:17 +0530

    Add files via upload

---
+++ Sudha_JS_lab3-master/weather-app/weather-api.js
@@ -0,0 +1,42 @@
+// Define a constant BASE_URL
+
+const API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+const APP_ID = "fbc0ee13d4cd0ca7ed7b116299399f34";
+const UNITS = "metric"
+
+// Define a constant API_ID
+
+// Define a constant for UNITS
+
+class WeatherAPI {
+
+  constructURL(userInput){
+
+    this.apiURL = new URL(API_BASE_URL);
+    
+    this.apiURL.searchParams.append("q", userInput);
+    this.apiURL.searchParams.append("appid", APP_ID);
+    this.apiURL.searchParams.append("units", UNITS);
+
+    console.log(this.apiURL.toString());
+  }
+
+
+  async invokeURL(){
+
+    const responseObj = await fetch(this.apiURL.toString());
+
+    const responseJSON = await responseObj.json();
+
+    return responseJSON;
+
+    // responseObj = await  fetch(url) // 1s / 10s
+    // responseJSON = await responseObj.json()
+
+    // return 
+    // print (json-response)
+  }
+
+}
+
+export {WeatherAPI}
---
+++ Sudha_JS_lab3-master/weather-app/weather-utils-test.js
@@ -0,0 +1,17 @@
+import { WeatherAPI } from "./weather-api.js";
+import { WeatherUtils } from "./weather-utils.js";
+
+async function testResponseConversion(){
+
+  const weatherAPIObj = new WeatherAPI();
+  weatherAPIObj.constructURL("France");
+  const responseJSON = await weatherAPIObj.invokeURL();
+
+  const responseObj = WeatherUtils.convertResponse(responseJSON);
+  // converResponse
+  // print (object)
+  console.log(responseObj);
+
+}
+
+testResponseConversion();
